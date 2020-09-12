f27107843bbe4e5ddeccbc6a2000741c49f8a901
Reyes, Michael <mradianlogs@gmail.com>
2020-09-12 17:42:25 -0500

    Began and near finished creating some templates for the front end of the project. Added a demo and logger to test adding data to the database in the main application class.

--- src/main/java/com/reyesProjects/ProjectPortfolio/AppController.java
+++ src/main/java/com/reyesProjects/ProjectPortfolio/AppController.java
@@ -1,35 +1,38 @@
 package com.reyesProjects.ProjectPortfolio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class AppController 
+public class AppController
 {
 	@Autowired
 	ProjectRepository projectDatabase;
+	
 	ProjectController projectController = new ProjectController(projectDatabase);
 	
 	@GetMapping("/home")
 	public String ShowHomePage()
 	{
-		return "homePage";
+		//return "homePage";
+		return "index";
 	}
 	
 	@GetMapping("/home/projects")
 	public String ShowAllProjectsPage(Model model)
 	{
-		model.addAllAttributes(projectController.getAllProjects()); //TODO: Find out how to access all of these guys in thymeleaf.
+		model.addAttribute( "projectsList", projectController.getAllProjects()); //TODO: Find out how to access all of these guys in thymeleaf.
 		return "allProjectsPage";
 	}
 	
 	@GetMapping("/home/projects/project")
 	public String ShowSpecificProjectPage(@RequestParam(name = "projectnum", required=true) int projectNum, Model model)
 	{
-		model.addAttribute("projectNum", projectController.getProject(projectNum));
+		model.addAttribute("project", projectController.getProject(projectNum));
 		return "specificProjectPage";
 	}
 }
--- src/main/java/com/reyesProjects/ProjectPortfolio/ProjectPortfolioWebAppApplication.java
+++ src/main/java/com/reyesProjects/ProjectPortfolio/ProjectPortfolioWebAppApplication.java
@@ -1,13 +1,55 @@
 package com.reyesProjects.ProjectPortfolio;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjectPortfolioWebAppApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(ProjectPortfolioWebAppApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectPortfolioWebAppApplication.class, args);
 	}
 
+	@Bean
+	  public CommandLineRunner demo(ProjectController projects) {
+	    return (args) -> {
+	      // save a few customers
+	      projects.createNewProject("Autocreated test project 1");
+	      projects.createNewProject("Autocreated test project 2");
+	      projects.createNewProject("Autocreated test project 3");
+	      projects.createNewProject("ATP 4");
+	      
+	      
+	      // Get all projects
+	      log.info("All Projects:");
+	      log.info("-------------------------------");
+	      for (Project project : projects.getAllProjects()) {
+	        log.info(project.toString());
+	      }
+	      log.info("");
+
+	      // Get project by Id.
+	      Project idProject = projects.getProject(1); //TODO: Test 0.
+	      log.info("Project with id 0:");
+	      log.info("--------------------------------");
+	      log.info(idProject.toString());
+	      log.info("");
+	      
+	      // Get project by name.
+	      Project nameProject = projects.getProject("Autocreated test project 2");
+	      log.info("Project with name Autocreated test project 2:");
+	      log.info("--------------------------------");
+	      log.info(nameProject.toString());
+	      log.info("");
+
+	      //TODO: Test multiple projects with same name.
+	      log.info("");
+	    };
+	}
 }
